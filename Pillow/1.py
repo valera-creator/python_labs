49c8594d50f0801ddf674021d74b95a66d0cb1c3
Valera <valerylarionov06@gmail.com>
2025-01-22 14:19:25 +0300

    фильтры a-d

--- Pillow/1.py
+++ Pillow/1.py
@@ -1,8 +1,41 @@
 # pip install pillow
+from PIL import Image
+
+
+def mirror_vertical(im):
+    im2 = im.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+    im2.save("vertical.jpg")
+
+
+def mirror_horizontal(im):
+    im2 = im.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+    im2.save("horizontal.jpg")
+
+
+def mirror_main_diagonal(im):
+    out = im.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+    out = out.transpose(Image.Transpose.ROTATE_270)
+    out.save("main_diagonal.jpg")
+
+
+def mirror_no_main_diagonal(im):
+    out = im.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+    out = out.transpose(Image.Transpose.ROTATE_270)
+    out.save('no_main_diagonal.jpg')
 
 
 def main():
-    pass
+    try:
+        im = Image.open("rayana.jpg")
+    except FileNotFoundError:
+        print('нет файла')
+        return
+
+    mirror_vertical(im)
+    mirror_horizontal(im)
+
+    mirror_main_diagonal(im)
+    mirror_no_main_diagonal(im)
 
 
 if __name__ == "__main__":
