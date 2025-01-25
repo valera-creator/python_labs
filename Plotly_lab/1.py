cee21a342abed584d10667663035ea0a3f0f1286
Valera <valerylarionov06@gmail.com>
2025-01-26 00:44:12 +0300

    plotly 1

--- Plotly_lab/1.py
+++ Plotly_lab/1.py
@@ -1,9 +1,25 @@
 # pip install plotly
 import plotly.express as px
+import pandas as pd
 
 
 def main():
-    pass
+    try:
+        df = pd.read_csv('data_country.csv', delimiter=',')
+    except FileNotFoundError:
+        print('нет файла')
+        return
+
+    fig = px.imshow(
+        df,
+        text_auto=True,  # значение в ячейках
+        aspect="auto",  # автоматическое подстроение соотношения сторон
+        title='Тепловая карта по странам',
+        labels=dict(x="Параметры", y="Страны", color="Значение"),  # подписи
+        y=df['country'],
+
+    )
+    fig.show()
 
 
 if __name__ == "__main__":
