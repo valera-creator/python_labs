3189eaf2fa3a5f908eb7b33aabbaa59b5ac7f546
Valera <valerylarionov06@gmail.com>
2025-01-26 02:15:42 +0300

    plotly 2a, 2b

--- Plotly_lab/2.py
+++ Plotly_lab/2.py
@@ -27,7 +27,44 @@ def display_cnt_courses(df):
     # Настройка отображения текста над значениями диаграммы
     fig.update_traces(textposition='outside')
 
-    # Показ диаграммы
+    fig.show()
+
+
+def display_cnt_subscribers(df):
+    is_paid = df[df['is_paid']]
+    not_is_paid = df[~df['is_paid']]
+
+    max_sub_paid = is_paid['num_subscribers'].max()
+    min_sub_paid = is_paid['num_subscribers'].min()
+    mean_sub_paid = is_paid['num_subscribers'].mean()
+
+    max_sub_not_paid = not_is_paid['num_subscribers'].max()
+    min_sub_not_paid = not_is_paid['num_subscribers'].min()
+    mean_sub_not_paid = not_is_paid['num_subscribers'].mean()
+
+    new_df = pd.DataFrame(
+        {
+            'Тип курса': ['Платные', 'Бесплатные'],
+            'Максимальное кол-во подписчиков': [max_sub_paid, max_sub_not_paid],
+            'Среднее кол-во подписчиков': [mean_sub_paid, mean_sub_not_paid],
+            'Минимальное кол-во подписчиков': [min_sub_paid, min_sub_not_paid]
+        }
+    )
+
+    # подписи и преобразования метрик
+    new_df_melted = new_df.melt(id_vars='Тип курса', var_name='легенда', value_name='Количество подписчиков')
+
+    # построение
+    fig = px.bar(
+        new_df_melted,
+        x='Тип курса',
+        y='Количество подписчиков',
+        color='легенда',  # максимальное, среднее, минимальное
+        barmode='group',  # Группировка столбцов
+        title='Сравнение платных и бесплатных курсов по количеству подписчиков',
+        text_auto=True  # Автоматическое отображение значений на столбцах
+    )
+
     fig.show()
 
 
@@ -39,6 +76,7 @@ def main():
         return
 
     display_cnt_courses(df)
+    display_cnt_subscribers(df)
 
 
 if __name__ == "__main__":
