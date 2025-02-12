e2f94a9a562c79af06ab2f0933486221981370b2
Valera <valerylarionov06@gmail.com>
2025-02-12 16:51:36 +0300

    разбил по функциям

--- повторение_2/2.py
+++ повторение_2/2.py
@@ -1,19 +1,24 @@
 import math
 
 
-def make_table():
+def get_n():
     try:
         n = int(input('введите число: '))
     except ValueError:
         print('не число')
-        return
+        quit()
 
     if n < 1:
         print('число меньше 1')
-        return
+        quit()
+
+    return n
+
+
+def make_table():
+    n = get_n()
 
     print('\t' + '\t'.join([str(i) for i in range(1, n + 1)]))
-
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if j == 1:
--- повторение_2/3.py
+++ повторение_2/3.py
@@ -1,4 +1,4 @@
-def main():
+def get_data():
     path = input('путь: ')
     try:
         with open(path, mode='r', encoding='utf-8') as file:
@@ -17,7 +17,22 @@ def main():
         quit()
 
     if not data:
-        return
+        quit()
+
+    return data
+
+
+def get_n():
+    try:
+        n = int(input('\nВведите число: '))
+        return n
+    except ValueError:
+        print('не число')
+        quit()
+
+
+def main():
+    data = get_data()
 
     data = sorted(data, key=lambda x: x[0])  # сортировка по имени
     print('Сортировка по имени:')
@@ -29,16 +44,12 @@ def main():
     for elem in data:
         print(f'{elem[0]} {elem[1]}')
 
-    try:
-        n = int(input('\nВведите число: '))
-    except ValueError:
-        print('не число')
-        quit()
+    n = get_n()
 
+    data = list(filter(lambda x: x[1] > n, data))
     with open('res.txt', mode='w', encoding='utf-8') as file:  # запись в файл
         for elem in data:
-            if elem[1] > n:
-                file.write(f'{elem[0]}\n')
+            file.write(f'{elem[0]}\n')
 
 
 if __name__ == "__main__":
